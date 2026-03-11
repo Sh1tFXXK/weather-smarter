f36285b3134b64a288d2d20cbe4cf539544a2cc0
Sh1tFXXK <3141479867@qq.com>
2026-03-11 23:53:33 +0800

    clear

--- config/__init__.py
+++
@@ -1,4 +0,0 @@
-# 配置模块初始化
-from .config import config, Config, DevelopmentConfig, TestingConfig, ProductionConfig
-
-__all__ = ['config', 'Config', 'DevelopmentConfig', 'TestingConfig', 'ProductionConfig']
